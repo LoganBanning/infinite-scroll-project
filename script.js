df070d913efeb7d005e6ab912a1e404e57f2b85b
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-04-13 17:52:32 -0600

    project complete

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 
 let photosArray = [];
 
@@ -8,16 +11,38 @@ const count = 10;
 const apiKey = 'wFxIbs5JsBmkBwJTF3HagUElLcANORKCSZ8c5zY0_Kc';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function imageLoaded(){
+  imagesLoaded++;
+  if(imagesLoaded === totalImages){
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+// Helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+  for(const key in attributes){
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Create Elements for Links & Photos, add to dom
 function displayPhotos(){
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
-    item.setAttribute('href', photo.links.html);
-    item.setAttribute('target', '_blank');
+    setAttributes(item, {
+      href: photo.links.html, 
+      target: '_blank',
+    });
     const img = document.createElement('img');
-    img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_discription,
+      title: photo.alt_discription,
+    });
+    img.addEventListener('load', imageLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -33,5 +58,13 @@ async function getPhotos(){
     //Catch error here
   }
 }
+
+// Check to see if scrolling near bottom or page, load more photos
+window.addEventListener('scroll', () => {
+  if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+    ready = false;
+    getPhotos();
+  }
+});
 
 getPhotos();
